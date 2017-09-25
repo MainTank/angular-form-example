df58509f78af239fa676c01f6efad084686347fb
Landen Wasserstrom <Landen.Wasserstrom@gmail.com>
2017-09-25 09:37:01 -0700

    two way data bind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,13 @@ export class AppComponent {
   title = 'app';
 
   onSubmit(data: NgForm){
-    console.log(data.value)
+    console.log(this.model)
   }
+
+model: object = {
+  first_name: "Landen",
+  last_name: "Wasserstrom"
+}
+
+
 }
